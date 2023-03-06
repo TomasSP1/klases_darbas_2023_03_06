88bcc74d84216c7d034d7edd9de312b66e507faa
TomasSP <113086584+TomasSP1@users.noreply.github.com>
2023-03-06 11:11:05 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Contacts from "./components/contacts/Contacts";
+
+function App() {
+  return (
+    <div className="contacts_card">
+      <Contacts/>
+    </div>
+  );
+}
+
+export default App;
